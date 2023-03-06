aee1d3d7db14a9ebd35a61f4e7c7624ef71e9233
kristian7777 <kristianjohannessen506@yahoo.com>
2023-03-06 10:09:06 +0100

    a

---
+++ javas.js
@@ -0,0 +1,123 @@
+var tall = ""
+var numberf = "123"
+        
+        function
+
+            farge1() {
+            
+            var tall = 1; 
+            var img1 = document.getElementById('valgtfarge1');
+            img1.src = 'fargebilde1.png';
+            
+
+        }
+        function farge2() {
+            
+            tall = "2";
+            var img2 = document.getElementById('valgtfarge1');
+            img2.src = 'fargebilde2.png';
+
+        }
+        function farge3() {
+            
+            onClick = tall = "3";
+            var img3 = document.getElementById('valgtfarge1');
+            img3.src = 'fargebilde3.png';
+
+        }
+        function farge4() {
+            onClick = tall = "4";
+            var img4 = document.getElementById('valgtfarge1');
+            img4.src = 'fargebilde4.png';
+
+        }
+        function farge5() {
+            onClick = tall = "5";
+            var img5 = document.getElementById('valgtfarge1');
+            img5.src = 'fargebilde5.png';
+
+        }
+        function farge6() {
+            onClick = tall = "6";
+            var img6 = document.getElementById('valgtfarge1');
+            img6.src = 'fargebilde6.png';
+            consol.log
+
+        }
+        function malingigrid(a)
+            {
+                var img55 = document.getElementById(a);
+                img55.src = 'fargebilde1.png';
+            }
+
+            
+            function myFunction() {
+                
+                document.getElementById("myText").innerHTML = tall;
+              }
+  
+
+/*var bildeValg = "1";
+
+
+
+function farge1() {
+    var bildeValg = "1";
+
+
+
+}
+function farge2() {
+    var bildeValg = "2";
+
+
+
+}
+function farge3() {
+    var bildeValg = "3";
+
+
+
+}
+function farge4() {
+    var bildeValg = "4";
+
+
+
+}
+function farge5() {
+    var bildeValg = "5";
+
+
+
+}
+function farge6() {
+    var bildeValg = "6";
+
+
+
+}
+
+function valgtfarge() {
+    console.log(bildeValg)
+    if (bildeValg === "1")
+        return document.getElementById("valgtfarge1").src = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde1.png";
+}
+if (bildeValg === "2") {
+    return document.getElementById("valgtfarge1").innerHTML = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde2.png";
+}
+if (bildeValg === "3") {
+    return document.getElementById("valgtfarge1").innerHTML = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde1.png";
+}
+if (bildeValg === "4") {
+    return document.getElementById("valgtfarge1").innerHTML = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde1.png";
+}
+if (bildeValg === "5") {
+    return document.getElementById("valgtfarge1").innerHTML = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde1.png";
+}
+if (bildeValg === "6") {
+    return document.getElementById("valgtfarge1").innerHTML = "C:\Users\genga\Desktop\github repository\fargebillder1-4\fargebilde1.png";
+}
+*/
+
+
